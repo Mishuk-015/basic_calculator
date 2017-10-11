04ac9d2d98349729e7e142f93e8a035b0f44fff7
Mishuk-015 <31391324+Mishuk-015@users.noreply.github.com>
2017-10-11 14:20:03 +0600

    Update Funcholder1.java

--- CALCULATOR1/src/Funcholder1.java
+++ CALCULATOR1/src/Funcholder1.java
@@ -1,5 +1,9 @@
-
 public class Funcholder1 {
+	private static Funcholder1 opreal=new Funcholder1();
+	public static Funcholder1 getinst()
+	{
+		return opreal;
+	}
 	public float add(float a,float b)
 	{
 		return a+b;
