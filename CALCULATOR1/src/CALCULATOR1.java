169783acabb7c912b77098df191de613c55ca006
Mishuk-015 <31391324+Mishuk-015@users.noreply.github.com>
2017-10-11 14:18:55 +0600

    Update CALCULATOR1.java

--- CALCULATOR1/src/CALCULATOR1.java
+++ CALCULATOR1/src/CALCULATOR1.java
@@ -1,13 +1,30 @@
 import java.util.Scanner;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class CALCULATOR1 {
 
 	public static void main(String[] args)
 	{
-		Funcholder1 op =new Funcholder1();
-		System.out.print("Enter 1 for addition,2 for subtraction,3 for division,4 for multiplication,5 for aquare of a number:");
+		float sum=0;
+		Funcholder1 op =Funcholder1.getinst();
+		System.out.print("Enter 1 for addition,2 for subtraction,3 for division,4 for multiplication,5 for aquare of a number,6 to add multiple numbers:");
 		Scanner in=new Scanner(System.in);
 		int operation=in.nextInt();
+		if(operation==6)
+		{
+			System.out.println("enter no of number you want to add:");
+			int k=in.nextInt();
+			System.out.println("enter the numbers:");
+			for(int i=0;i<k;i=i+1)
+			{
+				float j=in.nextFloat();
+				sum=op.add(sum, j);
+			}
+			System.out.println("SUM:"+sum);
+		}
+		else 
+		{
 		System.out.println("Enter 2 numbers:");
 		float a=in.nextFloat();
 		float b=in.nextFloat();
@@ -32,10 +49,6 @@ public class CALCULATOR1 {
 			System.out.println("Result of square of number 1:"+op.sqr(a));
 			System.out.println("Result of square of number 2:"+op.sqr(b));
 		}
-		else {
-			{
-				System.out.println("Not valid option");
-			}
 		}
 	}
 
